ca31f9c9cbae840f2c9bbed76aa98422a3052bec
Luiz Renato Rodrigues <lrenato10@hotmail.com>
2020-10-21 13:06:27 -0300

    alteracao das fontes e imagem EEG

--- Interface_IB1.py
+++ Interface_IB1.py
@@ -36,10 +36,12 @@ class Janela_Opcoes():
         self.opcoes.title('Janela de opções')
         self.opcoes['bg'] = '#524f4f'
         self.count=1
-        Label(self.opcoes, text='Selecione Uma das Opções',fg='white',bg= '#524f4f'  ).grid(row=0, column=0, columnspan=1, padx=10,pady=10)  # centraliza o label na coluna
-        Button(self.opcoes, text='Treinamento',width=20, bg='#0404B4',fg='white',command=Treinamento).grid(row=1, column=0, padx=50, pady=20)#cares criadas em https://html-color-codes.info/
-        Button(self.opcoes, text='Operação', width=20, bg='#0404B4',fg='white',command=Operacao).grid(row=2, column=0, padx=50, pady=20)
-        Button(self.opcoes, text='Instruções', width=20, bg='#0404B4',fg='white',command=Instrucoes).grid(row=3, column=0,padx=50,pady=20)
+        self.opcoes.resizable(True, True)
+        Label(self.opcoes, font=('helvetica',20), text='Selecione uma das opções',fg='white',bg= '#524f4f'  ).grid(row=0, column=0, columnspan=1, padx=10,pady=10) # centraliza o label na coluna
+        Button(self.opcoes, font=('helvetica',15),text='Treinamento',width=15, height=2, bg='#0404B4',fg='white',command=Treinamento).grid(row=1, column=0, padx=70, pady=40)#cares criadas em https://html-color-codes.info/
+        #self.pega.font(('helvetica', 50, 'underline italic')) 
+        Button(self.opcoes, font=('helvetica', 15), text='Operação', width=15, height=2, bg='#0404B4',fg='white',command=Operacao).grid(row=2, column=0, padx=70, pady=40)
+        Button(self.opcoes, font=('helvetica', 15), text='Instruções', width=15, height=2, bg='#0404B4',fg='white',command=Instrucoes).grid(row=3, column=0,padx=70,pady=40)
         print('test')
         self.opcoes.mainloop()
     
@@ -52,21 +54,23 @@ class Treinamento():
         self.treinamento.title('Treinamento')
         self.treinamento['bg'] = '#86cee4'
         
+        self.Lcontador=Label(self.treinamento, text='0',fg='black',bg= '#86cee4'  )
+        self.Lcontador.grid(row=3, column=0, columnspan=2, padx=10,pady=10)  # centraliza o label na coluna
+        
         self.gif_esq = ImageLabel(self.treinamento)
         self.gif_esq.grid(row=0, column=0)
-        self.gif_esq.load('esquerda.gif',10**8)
+        self.gif_esq.load('esquerda.gif',10**8,self.Lcontador)
         Label(self.treinamento, text='Mão Esquerda',fg='black',bg= '#86cee4'  ).grid(row=1, column=0, columnspan=1, padx=10,pady=10)  # posiciona leganda da mao esquerda
         
         self.gif_dir = ImageLabel(self.treinamento)
         self.gif_dir.grid(row=0, column=1)
-        self.gif_dir.load('direita.gif',10**8)
+        self.gif_dir.load('direita.gif',10**8,self.Lcontador)
         Label(self.treinamento, text='Mão Direita',fg='black',bg= '#86cee4'  ).grid(row=1, column=1, columnspan=1, padx=10,pady=10)  # posiciona leganda da mao direita
         
         
-        self.Lcontador=Label(self.treinamento, text='0',fg='black',bg= '#86cee4'  )
-        self.Lcontador.grid(row=3, column=0, columnspan=2, padx=10,pady=10)  # centraliza o label na coluna
+        
         Button(self.treinamento, text='Abrir EEG', width=20, bg='#0404B4',fg='white',command=AbrirEEG).grid(row=4, column=0,columnspan=2,padx=10,pady=30)
-        self.AtualizaContador()
+        #self.AtualizaContador()
         Button(self.treinamento, text='Esquerda', width=20, bg='#0404B4',fg='white',command=self.ApenasEsquerda).grid(row=5, column=0,columnspan=1,padx=10,pady=30)
         Button(self.treinamento, text='Direita', width=20, bg='#0404B4',fg='white',command=self.ApenasDireita).grid(row=5, column=1,columnspan=1,padx=10,pady=30)
         
@@ -94,14 +98,14 @@ class Treinamento():
         
     def ApenasDireita(self):
         self.gif_dir.unload()
-        self.gif_dir.load('direita.gif',100)
+        self.gif_dir.load('direita.gif',100,self.Lcontador)
         self.gif_esq.unload()
-        self.gif_esq.load('esquerda.gif',10**8)
+        self.gif_esq.load('esquerda.gif',10**8,self.Lcontador)
     def ApenasEsquerda(self):
         self.gif_dir.unload()
-        self.gif_dir.load('direita.gif',10**8)
+        self.gif_dir.load('direita.gif',10**8,self.Lcontador)
         self.gif_esq.unload()
-        self.gif_esq.load('esquerda.gif',100)
+        self.gif_esq.load('esquerda.gif',100,self.Lcontador)
         
 
 #================================================Janela de Operacao==================================================
@@ -131,11 +135,11 @@ class Instrucoes():
         #self.instrucoes.geometry('700x340')
         self.instrucoes.title('Instruções')
         self.instrucoes['bg'] = '#86cee4'
-        self.L1=Label(self.instrucoes, text='1/7',fg='white',bg= '#f29cc2'  )
+        self.L1=Label(self.instrucoes, text='1/7', font=('helvetica', 20), fg='black',bg= '#f29cc2'  )
         self.L1.pack()
-        self.L2=Label(self.instrucoes, text='Olá, este programa detecta qual mão você movimentou através de uma inteligência artificial! \n Para entender certinho como utilizar este programa, clique em "Próxima" \n e siga as instruções atentamente!',fg='black',bg= '#86cee4',width=80,height=20 )
+        self.L2=Label(self.instrucoes, font=('helvetica', 16), text='Olá, este programa detecta qual mão você movimentou através de uma inteligência artificial! \n Para entender certinho como utilizar este programa, clique em "Próxima" \n e siga as instruções atentamente!',fg='black',bg= '#86cee4',width=70,height=15 )
         self.L2.pack()
-        Button(self.instrucoes, text='Próxima', width=20, bg='#f29cc2',fg='black',command=self.Incremento).pack(side=RIGHT)
+        Button(self.instrucoes, text='Próximo', width=20, bg='#f29cc2',fg='black',command=self.Incremento).pack(side=RIGHT)
         Button(self.instrucoes, text='Anterior', width=20, bg='#f29cc2',fg='black',command=self.Decremento).pack(side=LEFT)
         Button(self.instrucoes, text='Eletrodos EEG', width=20, bg='#f8ef83',fg='black',command=self.EletrodosEEG).pack()
         self.instrucoes.mainloop()
@@ -143,38 +147,40 @@ class Instrucoes():
     def EletrodosEEG(self):
         self.instrucao3 = Toplevel()
         self.instrucao3.title('Posicionamento dos Eletrodos')
-        self.pic = 'eletrodo.png'
+        self.pic = 'EEG 10-20.png'
         self.pic1 = p.Image.open(self.pic)
         self.photo = ptk.PhotoImage(self.pic1)
+        self.instrucao3.resizable(False, False)
         labelImage = Label(self.instrucao3, image=self.photo)
         labelImage.grid(row=0, column=0)
         
         
     def Comparacao(self):
-        self.L1['text'] = '{}/7'.format(self.count)
+        self.L1['text'] = '{}/8'.format(self.count)
         if(self.count==1):
-            self.L2['text'] = 'Olá, este programa detecta qual mão você movimentou através de uma inteligência artificial! \n Para entender certinho como utilizar este programa, clique em "Próxima" \n e siga as instruções atentamente!'
+            self.L2['text'] = 'Olá, este programa detecta qual mão você movimentou através de uma inteligência artificial! \n Para entender certinho como utilizar este programa, clique em "Próximo" \n e siga as instruções atentamente!'
         elif (self.count==2):
             self.L2['text']='Conecte o microcontrolador a entrada USB do computador.'
         elif (self.count==3):
-            self.L2['text']='Coloque os eletrodos nas posições CZ, C3 e C4.\n Não se esqueça da referência!'
-            
+            self.L2['text']='Recomenda-se que o usuário abra a a imagem do posicionamento \n dos eletrodos de EEG clicando o botão amarelo \n\n\n\n Coloque os três eletrodos do sinal de intetesse (CZ, C3 e C4) \n Coloque o eletrodo de redução do artefato (Fz) \n Coloque o eletrodo de referência do circuito em uma das orelhas (A1 ou A2)'
         elif (self.count==4):
             self.L2['text']='Caso você queira treinar a inteligência artificial,\n clique em "Treinamento" no menu principal.\n'
         elif (self.count==5):
-            self.L2['text']='Siga com atenção as instruções da aba "Treinamentos"'
+            self.L2['text']='Siga com atenção as instruções da aba "Treinamentos".'
         elif (self.count==6):
-            self.L2['text']='Para visualizar o sinal eletroencefalográfico, vá em "Operação" no menu principal \n e selecione "Abrir EEG"'
+            self.L2['text']='Para visualizar o sinal eletroencefalográfico do treinamento, selecione "Abrir EEG".'
         elif (self.count==7):
             self.L2['text']='Para visualizar qual mão o usuário mexeu,\n vá em "Operação" no menu principal e siga as instruções.'
+        elif (self.count==8):
+            self.L2['text']='Para visualizar o sinal eletroencefalográfico da operação, selecione "Abrir EEG".'
         
     def Incremento(self):
-        if ((self.count >=1 )& (self.count <7)):
+        if ((self.count >=1 )& (self.count <8)):
             self.count=self.count+1
             self.Comparacao()
         
     def Decremento(self):
-        if ((self.count >1) & (self.count <=7)):
+        if ((self.count >1) & (self.count <=8)):
             self.count=self.count-1
             self.Comparacao()
         
--- gerargif.py
+++ gerargif.py
@@ -6,7 +6,7 @@ from itertools import count
 
 class ImageLabel(tk.Label):
     """a label that displays images, and plays them if they are gifs"""    
-    def load(self, im,atraso):
+    def load(self, im,atraso,objeto):
         if isinstance(im, str):
             im = Image.open(im)
         self.count = 0
@@ -28,25 +28,26 @@ class ImageLabel(tk.Label):
         if len(self.frames) == 1:
             self.config(image=self.frames[0])
         else:
-            self.next_frame()
+            self.next_frame(objeto)
 
     def unload(self):
         self.config(image="")
         self.frames = None
 
-    def next_frame(self):
+    def next_frame(self,objeto):
         if self.frames:
             self.loc += 1# de quanto em quantos frames
             self.loc %= len(self.frames)# reinicia o vetor do frame quando ele chega no final
-            self.config(image=self.frames[self.loc])
+            self.config(image=self.frames[self.loc])#atualiza a a imagem do frame
             self.after(self.delay, self.next_frame)
             if self.loc%len(self.frames)==0:
                 self.count+=1
                 self.imprime_contador()
                 print(self.count)
+                objeto['text']=str(self.count)
                 
-    def imprime_contador(self):
-        return self.count
+   # def imprime_contador(self):
+    #    return self.count
 
 """#root = tk.Tk()
 lbl = ImageLabel(root)
